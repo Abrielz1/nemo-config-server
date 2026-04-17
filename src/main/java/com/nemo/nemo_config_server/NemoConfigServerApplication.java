ce85ff22a07b6849ca645551d3919facad8cec47
Abriel <106871954+Abrielz1@users.noreply.github.com>
2026-04-17 14:13:22 +0300

    Add files via upload

---
+++ src/main/java/com/nemo/nemo_config_server/NemoConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.nemo.nemo_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NemoConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NemoConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nemo/nemo_config_server/NemoConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nemo.nemo_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NemoConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
